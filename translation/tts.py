0feb9ac6963c71090c1c20f06ced3f4bb9d2a596
LM1398 <leomatsu13@gmail.com>
2021-12-20 13:13:13 +0900

    [check] adding error to tts.py to see if flake annotations work

--- translation/tts.py
+++ translation/tts.py
@@ -7,6 +7,7 @@ from local_settings import IBM_WATSON_APIKEY
 import playsound
 import os
 import yaml
+import time
 
 with open("./envs/config.yml") as file:
     config = yaml.safe_load(file.read())
